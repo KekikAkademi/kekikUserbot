145cbeabcc80b806eac814ce556991f55f1e8cc9
M.Furkan <byazade@yandex.com>
2020-09-14 23:55:28 +0300

    telethona dönüştür ver: 0.1

--- roBot/_edevat/zenginLog.py
+++ roBot/_edevat/zenginLog.py
@@ -1,39 +1,42 @@
 # Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
-
+import telethon
+from telethon import events, client
+from telethon.tl.functions.users import GetFullUserRequest
+# from pyrogram import Client
+# from pyrogram.types import Message
 from roBot._edevat import logVer
-import json, datetime, pytz
-
-tarih = datetime.datetime.now(pytz.timezone("Turkey")).strftime("%d-%m-%Y") # Bugünün Tarihi
-saat = datetime.datetime.now(pytz.timezone("Turkey")).strftime("%H:%M")     # Bugünün Saati
+import json
+from kekikTaban._evrensel import saat, tarih
 
 bilgiler = json.load(open("bilgiler.json"))
 
-async def logYolla(client, message):
+async def logYolla(event) -> bool:
     # < LOG Alanı
     log_dosya = f"[{saat} / {tarih}] "
-    # sohbet = await client.get_chat(message.chat.id)
+    sohbet = await event.client(GetFullUserRequest(event.from_id))
+    chat = await event.client.get_chat()
     
-    if message.from_user.username:
-        log_mesaj   = f"@{message.from_user.username}"
-        log_konsol  = f"[bold red]{message.from_user.username}[/] [green]||[/] "
-        log_dosya  += f"{message.from_user.username} || "
+    if sohbet.username:
+        log_mesaj   = f"@{sohbet.username}"
+        log_konsol  = f"[bold red]{sohbet.username}[/] [green]||[/] "
+        log_dosya  += f"{sohbet.username} || "
     else:
-        log_mesaj   = f"[{message.from_user.first_name}](tg://user?id={message.from_user.id})"
-        log_konsol  = f"[bold red]{message.from_user.first_name}[/] "
-        log_dosya   = f"{message.from_user.first_name} "
+        log_mesaj   = f"[{sohbet.first_name}](tg://user?id={sohbet.id})"
+        log_konsol  = f"[bold red]{sohbet.first_name}[/] "
+        log_dosya   = f"{sohbet.first_name} "
     
-    if message.chat.type != 'private' and message.chat.type != 'bot':
-        log_mesaj   += f"\n\n\t\t`{sohbet.title}`__'den__ `{message.text}` __yolladı..__"
-        log_konsol  += f"[yellow]{message.text}[/] [green]||[/] [bold cyan]{sohbet.title}[/] "
-        log_dosya   += f"{message.text} || {sohbet.title} "
+    if event.is_group != True and event.is_bot != True:
+        log_mesaj   += f"\n\n\t\t`{chat.title}`__'den__ `{event.message.message}` __yolladı..__"
+        log_konsol  += f"[yellow]{event.message.message}[/] [green]||[/] [bold cyan]{chat.title}[/] "
+        log_dosya   += f"{event.message.message} || {chat.title} "
     else:
-        log_mesaj   += f"\n\n\t\t`{message.text}` __yolladı..__"
-        log_konsol  += f"[yellow]{message.text}[/] "
-        log_dosya   += f"{message.text} "
+        log_mesaj   += f"\n\n\t\t`{event.message.message}` __yolladı..__"
+        log_konsol  += f"[yellow]{event.message.message}[/] "
+        log_dosya   += f"{event.message.message} "
     
-    log_mesaj   +=  f"\n\n**Sohbet Türü :** __{message.chat.type}__"
-    log_konsol  += f"\t[green]||[/] [magenta]{message.chat.type}[/]"
-    log_dosya   += f"\t|| {message.chat.type}\n"
+#     log_mesaj   +=  f"\n\n**Sohbet Türü :** __{message.chat.type}__"
+#     log_konsol  += f"\t[green]||[/] [magenta]{message.chat.type}[/]"
+#     log_dosya   += f"\t|| {message.chat.type}\n"
 
     # await client.send_message(bilgiler['log_id'], log_mesaj)                  # log_id'ye log gönder
     logVer(f"{log_konsol}")                                                   # zenginKonsol'a log gönder
